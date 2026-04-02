1b2cea9d7d3b76464b0ae45cf85fb9d23b1d5a2f
STEPS0N <stepan.klimoff@mail.ru>
2026-04-02 08:57:42 +0500

    Добавьте файлы проекта.

---
+++ Word_Klimov/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Word_Klimov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
